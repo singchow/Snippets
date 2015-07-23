6a4d2ed55f8f4f8991fda1a9aa2190855241ef65
jjmok <jjmok89@gmail.com>
2015-07-23 22:06:45 +0800

    added landingpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'welcome' => 'landing#landingpage'
+  root 'landing#landingpage'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
