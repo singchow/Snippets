23b6e03be2acc898739188eaffde4dffe795c090
dylansiauw <dylansiauw@gmail.com>
2015-07-26 16:21:00 +0800

    added username into database

--- db/migrate/20150722122703_create_users.rb
+++ db/migrate/20150722122703_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.integer :user_id
+      t.string :username
       t.string :first_name
       t.string :last_name
       t.string :email
