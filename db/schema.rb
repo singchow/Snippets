106c27f9658189ecc60ad08179587ee906c896cd
Shiung <hh.shiung@gmail.com>
2015-07-28 09:46:26 +0800

    Schema file had changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724085326) do
+ActiveRecord::Schema.define(version: 20150727095700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "snippets", force: :cascade do |t|
     t.string   "snippet_title"
@@ -27,9 +32,6 @@ ActiveRecord::Schema.define(version: 20150724085326) do
 
   create_table "users", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "username"
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "email"
     t.string   "password"
     t.boolean  "is_email_confirm"
