64f6a66ad2b4451bcf46bd6ea34717507e5261de
singchow <sing.chow.ching@outlook.com>
2015-07-30 14:18:24 +0800

    enhance index,show,_form for user for username,first_name,last_name and removal of user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726114341) do
+ActiveRecord::Schema.define(version: 20150730035625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20150726114341) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer  "user_id"
     t.string   "email"
     t.string   "password"
     t.boolean  "is_email_confirm"
