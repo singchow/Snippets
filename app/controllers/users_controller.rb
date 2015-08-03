7c46d7785c94fb8c1bcd8261bc4032587a062e25
singchow <sing.chow.ching@outlook.com>
2015-08-03 21:37:14 +0800

    redirect user after registration to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,15 +46,20 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+    redirect_to "/login"
     end
+
+    # respond_to do |format|
+    #   if @user.save
+    #     # format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #     # format.json { render :show, status: :created, location: @user }
+    #     render template: 'users/login'
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /users/1
