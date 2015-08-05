5baa17540ea114a7cd386359e1726dc4f276873c
singchow <sing.chow.ching@outlook.com>
2015-08-05 20:24:15 +0800

    Fixed bu to use params first then only cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,12 @@ class UsersController < ApplicationController
   def showIndex
     # Snippets::Application::MaxPostInADay
     # Refer to config/application.rb for Global Static Variable
+    if (params[:email] != nil)
+      cookies[:current_user_email] = params[:email]
+    end
 
     if(User.exists?(email: cookies[:current_user_email]))
-      if (params[:email] != nil)
-        cookies[:current_user_email] = params[:email]
-      end
+      # Do nothing. Continue
     else
       flash[:invaliduser] = "#{params[:email]} does not exist. Have you registered?"
       redirect_to "/login" and return
@@ -42,6 +43,7 @@ class UsersController < ApplicationController
 
     @personaluserid =  User.find_by(email: cookies[:current_user_email])
     puts @personaluserid.email
+    puts @personaluserid.username
     @personalsnippets = Snippet.all.where(user_id: @personaluserid.id)
     render template: 'users/personal'
   end
