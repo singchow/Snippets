a04445318ec02efad2fc553c4cfad8c369344d2a
singchow <sing.chow.ching@outlook.com>
2015-08-07 17:41:46 +0800

    bug with register page fixed

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 20150806184541) do
   add_index "snippets", ["user_id"], name: "index_snippets_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email",                  default: "", null: false
     t.string   "password"
     t.boolean  "is_email_confirm"
@@ -71,9 +74,6 @@ ActiveRecord::Schema.define(version: 20150806184541) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "username"
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
