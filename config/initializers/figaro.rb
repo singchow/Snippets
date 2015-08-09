99ba77668d63087c60c68a0a57fc4dcc061d5415
singchow <sing.chow.ching@outlook.com>
2015-08-09 15:52:11 +0800

    remove dropbox_access_type

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,1 +1,1 @@
-Figaro.require_keys("dropbox_app_key", "dropbox_app_secret", "dropbox_access_token", "dropbox_access_token_secret", "dropbox_user_id", "dropbox_access_type")
+Figaro.require_keys("dropbox_app_key", "dropbox_app_secret", "dropbox_access_token", "dropbox_access_token_secret", "dropbox_user_id")
