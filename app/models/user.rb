78edf5003120e00cc70ff8dc0b73d8cc93e4fcca
singchow <sing.chow.ching@outlook.com>
2015-08-09 15:01:41 +0800

    used figaro to sore env variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, :styles => { :medium => "90x90>", :thumb => "30x30>" }, :default_url => ":styles/missing.jpg"
+  # has_attached_file :avatar, :styles => { :medium => "90x90>", :thumb => "30x30>" }, :default_url => ":styles/missing.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  #added for paperclip-dropbox gem
+has_attached_file :picture,
+ :storage => :dropbox,
+:dropbox_credentials => "#{Rails.root}/config/dropbox_c
+onfig.yml",
+:default_url => ":styles/missing.jpg",
+ :styles => { :medium => "300x300" , :thumb => "100x100>"},
+:dropbox_options => {
+:path => proc { |style| "#{Rails.env}/#{style}/#{id}_#{picture.original_filename}"},:unique_filename => true
+  }
 
   # Associate many Snippets
   has_many :snippets, dependent: :destroy
