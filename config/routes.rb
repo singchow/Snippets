eefadb4722bea1e20ba4046f38002e5a8ea82996
Shiung <hh.shiung@gmail.com>
2015-08-09 18:15:03 +0800

    updated text on landingpage, configured snippets to reference from the new layout file, formatted routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,21 @@ Rails.application.routes.draw do
 
   get '/index' => 'users#showIndex'
   post '/index' => 'users#showIndex'
+
   get '/login' => 'users#showLogin'
   get '/lock' => 'users#showLock'
   get '/logout' => 'users#showLogout'
   get '/register' => 'users#new'
   get '/personal' => 'users#showPersonal'
+
   get '/fav' => 'users#showFav'
+
   get '/performance' => 'users#showPerformance'
+
   get '/confirmation/:token' , to: 'users#verifyToken'
+
   get '/test' => 'users#sendEmail'
+
   post '/getinput' => 'users#getinput'
   post '/forgotpw' => 'users#forgotPW'
 
