24b89ba580dce5ee67ae2e841ad3fbc73240f9ca
singchow <sing.chow.ching@outlook.com>
2015-08-18 15:58:34 +0800

    create favorite resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :favorites
   # devise_for :users
   resources :favorites
   resources :snippets
--- db/migrate/20150818075739_create_favorites.rb
+++ db/migrate/20150818075739_create_favorites.rb
@@ -1,6 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
+      t.string :user_id
+      t.string :snippet_id
 
       t.timestamps null: false
     end
