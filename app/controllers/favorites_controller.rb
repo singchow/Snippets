eee9569c6caeafe2914e555721416f2730f44207
singchow <sing.chow.ching@outlook.com>
2015-08-18 16:40:06 +0800

    added favorite ui element

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,7 @@
 class FavoritesController < ApplicationController
+  layout 'dashboard_layout'
+
+  def index
+  end
+
 end
--- db/migrate/20150818075739_create_favorites.rb
+++ db/migrate/20150818075739_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
-      t.string :user_id
-      t.string :snippet_id
+      t.integer :user_id
+      t.integer :snippet_id
 
       t.timestamps null: false
     end
