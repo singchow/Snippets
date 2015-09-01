9794a0a66972fff395ebf99c53f4d209a60f95d4
Shiung <hh.shiung@gmail.com>
2015-09-01 20:31:51 +0800

    Updated database with db:seed; moved create snippet button to be aligned with feedback; updated personal log

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818081013) do
+ActiveRecord::Schema.define(version: 20150821074146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 20150818081013) do
   end
 
   add_index "snippets", ["user_id"], name: "index_snippets_on_user_id", using: :btree
+
+  create_table "userfeedbacks", force: :cascade do |t|
+    t.string   "email"
+    t.string   "feedback"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
